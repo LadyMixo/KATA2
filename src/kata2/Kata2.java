91b541c90b465568eba388a94d196c3e8423e2ce
LaraVieraLara <LaraVieraLara@Mixo_Skynet>
2016-06-06 13:31:31 +0100

    Kata2

---
+++ src/kata2/Kata2.java
@@ -0,0 +1,11 @@
+package Kata2;
+
+public class Kata2 {  
+    public static void main(String[] args) {
+        Integer[] array = {1,2,4,5,2,5,2,234,2,32,3,23,2-213,12};
+        String[] array2 = {"Ana","Juan","Pedro","Luis","Luis","Ana"};
+
+        Histogram <Integer> histogram = CalculaHistogram.computeHisto(array);
+        for (Object key: histogram.keySet()) System.out.println(key+" : "+histogram.get(key)+" veces");
+    }
+}
